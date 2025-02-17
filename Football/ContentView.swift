cdf8af8b72fe8f3f3587f4b8339544fb770b13aa
Bluetenhonig <linda.samsinger@uzh.ch>
2025-02-17 10:38:15 +0100

    no comment

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                     .resizable()
                     .scaledToFit()
                     .foregroundStyle(.purple)
-                // lol
+                
             }
         }
         .padding()
